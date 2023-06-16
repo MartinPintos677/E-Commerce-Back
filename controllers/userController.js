5729f1eb41974caf8e2d3e04496b5837daec33dd
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-06-16 13:12:07 -0300

    Prototipo UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,88 @@
-const { User } = require("../models");
+const { User, Order } = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: "Incorrect credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Incorrect credentials" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: "10h" });
+
+    return res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Internal server error" });
+  }
+}
+
+async function signUp(req, res) {
+  try {
+    const { firstname, lastname, email, password, address, phone } = req.body;
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+      address,
+      phone,
+    });
+
+    /*const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "10h",
+    });*/
+
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ message: "Internal server error" });
+  }
+}
+
+async function logout(req, res) {
+  try {
+    res.clearCookie("token");
+    console.log("logged out successfully");
+    return res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+async function getOrders(req, res) {
+  try {
+    const userId = req.user.userId; // Assuming you have middleware to extract the user ID from the token
+
+    const user = await User.findByPk(userId, { include: Order });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ orders: user.orders });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 // Display a listing of the resource.
 async function index(req, res) {}
@@ -32,4 +116,8 @@ module.exports = {
   edit,
   update,
   destroy,
+  login,
+  signUp,
+  logout,
+  getOrders,
 };
