f4476829eab36935ca72174f765c72fa2290c6e2
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-07-15 20:54:05 -0300

    category update (por la foto)

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -109,7 +109,7 @@ async function store(req, res) {
 }
 
 // Update the specified resource in storage.
-async function update(req, res) {
+/*async function update(req, res) {
   try {
     const { id } = req.params;
     const form = formidable({
@@ -129,6 +129,63 @@ async function update(req, res) {
       }
 
       const category = await Category.findByPk(id);
+
+      if (!category) {
+        return res.status(404).json({ message: "Category not found" });
+      }
+
+      await category.update(categoryUpdate);
+
+      return res.status(200).json(category);
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}*/
+
+async function update(req, res) {
+  try {
+    const form = formidable({
+      multiples: true,
+      keepExtensions: true,
+    });
+
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      const categoryId = req.params.id;
+
+      const categoryUpdate = {
+        name: fields.name.toString(),
+        description: fields.description.toString(),
+      };
+
+      if (files.image) {
+        const ext = path.extname(files.image.filepath);
+        const newFileName = `image_${Date.now()}${ext}`;
+
+        const { data, error } = await supabase.storage
+          .from("images")
+          .upload(newFileName, fs.createReadStream(files.image.filepath), {
+            cacheControl: "3600",
+            upsert: false,
+            contentType: files.image.mimetype,
+            duplex: "half",
+          });
+
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ message: "Internal server error" });
+        }
+
+        categoryUpdate.image = newFileName;
+      }
+
+      const category = await Category.findByPk(categoryId);
 
       if (!category) {
         return res.status(404).json({ message: "Category not found" });
