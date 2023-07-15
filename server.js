2f7f7b88df010cf26fd6d9026ace4aed1cf6d4fc
Martin Pintos <102275218+MartinPintos677@users.noreply.github.com>
2023-07-15 01:10:19 -0300

    prueba v13

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const methodOverride = require("method-override");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
-//app.use(cors());
-const corsOptions = {
-  origin: "https://admin-front-topaz.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  optionsSuccessStatus: 200, // Algunas versiones de CORS requieren esto para que funcione correctamente
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
